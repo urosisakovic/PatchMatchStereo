054bf38601a8ea749ad69886941ee611c18ba794
urosisakovic <uros.isakovic98@gmail.com>
2020-10-03 21:56:21 +0200

    Added a bunch of to-be-filled, not working code :)

--- patch_match.py
+++ patch_match.py
@@ -2,8 +2,52 @@ import numpy as np
 from PIL import Image
 
 
+def pixel_weights(image_left, image_right):
+    pass
+
+
+def greyscale_gradient(image):
+    pass
+
+
+def initialize_random_planes(image_left, image_right):
+    pass
+
+
+def patch_match_process():
+    pass
+
+
+def patch_match_postprocess(disparity_left, disparity_right):
+    pass
+
+
 def patch_match_stereo(image_left, image_right):
-    return np.zeros(image_left.shape), np.zeros(image_left.shape)
+    # Compute interpixel weights
+    pixel_weights_left, pixel_weights_right = pixel_weights(image_left, image_right)
+
+    # Compute grayscale gradients
+    gradient_left = greyscale_gradient(image_left)
+    gradient_right = greyscale_gradient(image_right)
+
+    # Intialize random planes
+    planes_left, planes_right = initialize_random_planes(image_left, image_right)
+
+    # Evalute initial plane cost
+    
+    # PatchMatch processing
+    patch_match_process()
+
+    # Post processing
+    disparity_left = disparity_from_planes(planes_left)
+    disparity_right = disparity_from_planes(planes_right)
+    patch_match_postprocess(disparity_left, disparity_right)
+
+    # Compute final disparity and return it
+    disparity_left = disparity_from_planes(planes_left)
+    disparity_right = disparity_from_planes(planes_right)
+
+    return disparity_left, disparity_right
 
 
 if __name__ == "__main__":
@@ -37,25 +81,24 @@ if __name__ == "__main__":
     # Find left and right disparities
     disparity_left, disparity_right = patch_match_stereo(image_left, image_right)
 
-    # Scale disparities and switch to unsigned single-byte integer type
+    # Scale left disparity image
     if disparity_left.max() - disparity_left.min() != 0:
         disparity_left = (disparity_left - disparity_left.min()) / \
             (disparity_left.max() - disparity_left.min())
         disparity_left *= 255
-    disparity_left = disparity_left.astype(np.uint8)
 
+    # Scale right disparity image
     if disparity_right.max() - disparity_right.min() != 0:
         disparity_right = (disparity_right - disparity_right.min()) / \
             (disparity_right.max() - disparity_right.min())
         disparity_right *= 255
-    disparity_right = disparity_right.astype(np.uint8)
 
     # Save disparities as images
     try:
-        disparity_left_image = Image.fromarray(disparity_left)
+        disparity_left_image = Image.fromarray(disparity_left.astype(np.uint8))
         disparity_left_image.save(disparity_left_filepath)
 
-        disparity_right_image = Image.fromarray(disparity_right)
+        disparity_right_image = Image.fromarray(disparity_right.astype(np.uint8))
         disparity_right_image.save(disparity_right_filepath)
     except:
         print("Failed to save images.")
