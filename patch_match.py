15a1127685dd8f94d4bb5c3e62764bc479b0a7e6
urosisakovic <uros.isakovic98@gmail.com>
2020-10-03 22:31:23 +0200

    Filled pixel_weights and initialize_random_planes functions. Added hyperparameter dict.

--- patch_match.py
+++ patch_match.py
@@ -2,16 +2,51 @@ import numpy as np
 from PIL import Image
 
 
-def pixel_weights(image_left, image_right):
-    pass
+class Plane:
+    
+    def __init__():
+        self.point_x = 0
+        self.point_y = 0
+        self.point_z = 0
+
+        self.normal_x = 0
+        self.normal_y = 0
+        self.normal_z = 0
+
+
+def pixel_weights(image, window_size, gamma):
+    weights = [[{} for _ in range(image.shape[1])] for _ in range(image.shape[0])]
+
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+
+            for y in range(row - window_size // 2, row + window_size // 2 + 1):
+                for x in range(col - window_size // 2, col + window_size // 2 + 1):
+                    if 0 <= y < image.shape[0] and 0 <= x < image.shape[1]:
+                        weights[row][col][y, x] = np.exp(np.abs(image[row, col] - image[y, x]) / gamma)
+
+    return weights
 
 
 def greyscale_gradient(image):
     pass
 
 
-def initialize_random_planes(image_left, image_right):
-    pass
+def initialize_random_planes(shape, max_disparity):
+    
+    planes = [[Plane() for _ in range(shape[1])] for _ in range(shape[0])]
+
+    for row in range(shape[0]):
+        for col in range(shape[1]):
+            planes[row][col].point_x = col
+            planes[row][col].point_y = row
+            planes[row][col].point_z = np.random.uniform(0, max_disparity)
+
+            planes[row][col].normal_x = np.random.uniform(np.iinfo(np.int32).min, np.iinfo(np.int32).max)
+            planes[row][col].normal_y = np.random.uniform(np.iinfo(np.int32).min, np.iinfo(np.int32).max)
+            planes[row][col].normal_z = np.random.uniform(np.iinfo(np.int32).min, np.iinfo(np.int32).max)
+
+    return planes
 
 
 def patch_match_process():
@@ -22,16 +57,26 @@ def patch_match_postprocess(disparity_left, disparity_right):
     pass
 
 
-def patch_match_stereo(image_left, image_right):
+def patch_match_stereo(image_left, image_rightm, hyperparameters):
     # Compute interpixel weights
-    pixel_weights_left, pixel_weights_right = pixel_weights(image_left, image_right)
+    pixel_weights_left = pixel_weights(
+        image_left,
+        hyperparameters["window_size"],
+        hyperparameters["gamma"]
+    )
+    pixel_weights_right = pixel_weights(
+        image_right,
+        hyperparameters["window_size"],
+        hyperparameters["gamma"]
+    )
 
     # Compute grayscale gradients
     gradient_left = greyscale_gradient(image_left)
     gradient_right = greyscale_gradient(image_right)
 
     # Intialize random planes
-    planes_left, planes_right = initialize_random_planes(image_left, image_right)
+    planes_left = initialize_random_planes(image_left.shape, hyperparameters["max_disparity"])
+    planes_right = initialize_random_planes(image_right.shape, hyperparameters["max_disparity"])
 
     # Evalute initial plane cost
     
@@ -52,10 +97,14 @@ def patch_match_stereo(image_left, image_right):
 
 if __name__ == "__main__":
     # Hyperparameters
-    alpha = 0.9
-    gamma = 10.0
-    tau_c = 10.0
-    tau_g = 2.0
+    hyperparameters = {
+        "alpha": 0.9,
+        "gamma": 10.0,
+        "tau_c": 10.0,
+        "tau_g": 2.0,
+        "max_disparity": 60,
+        "window_size": 35
+    }
 
     # Input image filepaths
     image_left_filepath = "images\\aloe_left.png"
@@ -67,8 +116,8 @@ if __name__ == "__main__":
 
     # Load images
     try:
-        image_left = np.array(Image.open(image_left_filepath))
-        image_right = np.array(Image.open(image_right_filepath))
+        image_left = np.array(Image.open(image_left_filepath).convert('LA'))
+        image_right = np.array(Image.open(image_right_filepath).convert('LA'))
     except:
         assert False, "Invalid image filepaths."
 
@@ -79,7 +128,7 @@ if __name__ == "__main__":
         assert dim_left == dim_right, "Invalid image sizes."
 
     # Find left and right disparities
-    disparity_left, disparity_right = patch_match_stereo(image_left, image_right)
+    disparity_left, disparity_right = patch_match_stereo(image_left, image_right, hyperparameters)
 
     # Scale left disparity image
     if disparity_left.max() - disparity_left.min() != 0:
